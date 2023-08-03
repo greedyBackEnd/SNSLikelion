50bf5945822e46e427eb85d1446eafd04dfbc29b
kdybe01 <kdybe01@gmail.com>
2023-08-03 13:51:53 +0900

    Initial commit

---
+++ src/main/java/com/example/likelionSNS/LikelionSnsApplication.java
@@ -0,0 +1,13 @@
+package com.example.likelionSNS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LikelionSnsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LikelionSnsApplication.class, args);
+	}
+
+}
