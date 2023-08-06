2161e431d9a10a7cc93585e05b3340bc8fecbeba
kdybe01 <kdybe01@gmail.com>
2023-08-06 10:01:26 +0900

    feat : 파일 업로더 네이밍 수정 - 업로드 파일 네이밍 : timeStamp + UUID + originalFilename

--- src/main/java/com/example/likelionSNS/utils/FileUploadUtils.java
+++ src/main/java/com/example/likelionSNS/utils/FileUploadUtils.java
@@ -12,26 +12,30 @@ import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.UUID;
 
 @Slf4j
 public class FileUploadUtils {
-
     // 이미지 저장
     public static String saveFile(String uploadDir, String originalFileName, MultipartFile multipartFile) throws IOException {
         String extension = StringUtils.getFilenameExtension(originalFileName);
+        String baseName = StringUtils.getFilename(originalFileName);
         String timeStamp = new SimpleDateFormat("yyyy_MM_dd_HH_mm_ss").format(new Date());
-        String fileName = timeStamp + ((extension != null) ? "." + extension : "");
 
-        Path uploadPath = Paths.get(uploadDir, fileName);
+        // filename = timeStamp + UUID + originalFilename
+        String fileName = timeStamp + "_" + UUID.randomUUID() + "_" + baseName + ((extension != null) ? "." + extension : "");
 
-        if (!Files.exists(uploadPath)) {
-            Files.createDirectories(uploadPath);
+        Path dirPath = Paths.get(uploadDir);
+        Path uploadPath = dirPath.resolve(fileName);
+
+        if (!Files.exists(dirPath)) {
+            Files.createDirectories(dirPath);
         }
         try (InputStream inputStream = multipartFile.getInputStream()) {
             Files.copy(inputStream, uploadPath, StandardCopyOption.REPLACE_EXISTING);
         } catch (IOException ioe) {
             throw new IOException("이미지를 저장할 수 없습니다 : " + fileName, ioe);
         }
-        return String.format("/" + uploadDir + "/" + fileName);
+        return String.format(uploadDir + "/" + fileName);
     }
 }
