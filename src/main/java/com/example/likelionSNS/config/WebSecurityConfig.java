37bfbe8344c680ded6b665850b5193edd482cfe2
kdybe01 <kdybe01@gmail.com>
2023-08-06 14:16:16 +0900

    feat : 피드 단독 조회, 전체 조회 시큐리티 접근 설정 추가 - WebSecurityConfig.java "/api/v1/feeds/**", "/api/v1/feeds/user/**" 해당 경로 get매핑 permitAll

--- src/main/java/com/example/likelionSNS/config/WebSecurityConfig.java
+++ src/main/java/com/example/likelionSNS/config/WebSecurityConfig.java
@@ -3,6 +3,7 @@ package com.example.likelionSNS.config;
 import com.example.likelionSNS.jwt.JwtTokenFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -10,7 +11,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.intercept.AuthorizationFilter;
-import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 
 @Configuration
 public class WebSecurityConfig {
@@ -30,6 +30,8 @@ public class WebSecurityConfig {
                         authHttp -> authHttp
                                 .requestMatchers("/api/v1/auth/**", "/token/issue").permitAll()
                                 .requestMatchers("/api/v1/users/**").authenticated()
+                                .requestMatchers( HttpMethod.GET,"/api/v1/feeds/**").permitAll()
+                                .requestMatchers( HttpMethod.GET,"/api/v1/feeds/user/**").permitAll()
 //                                .requestMatchers(HttpMethod.GET, "/api/v2/items/**").permitAll()
                                 .anyRequest().authenticated()
 //                                .anyRequest().permitAll()
