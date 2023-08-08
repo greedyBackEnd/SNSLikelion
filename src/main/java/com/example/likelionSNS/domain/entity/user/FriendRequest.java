2bfd3a5c6213e78c720cdf4bdcfd206bdf5fe2f6
kdybe01 <kdybe01@gmail.com>
2023-08-08 15:01:56 +0900

    feat : 친구 요청을 관리하기 위한 엔티티 작성 - FriendRequest.java

---
+++ src/main/java/com/example/likelionSNS/domain/entity/user/FriendRequest.java
@@ -0,0 +1,28 @@
+package com.example.likelionSNS.domain.entity.user;
+
+import com.example.likelionSNS.domain.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@SuperBuilder
+@Table(name = "friend_request")
+public class FriendRequest extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "requester_id")
+    private User requester;
+
+    @ManyToOne
+    @JoinColumn(name = "receiver_id")
+    private User receiver;
+}
